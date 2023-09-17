888a83424ee300cf8d133e9cc6a603e051e6fed9
meyvofan <cookingfolney@gmail.com>
2023-09-17 13:03:19 +0300

    Дз сделано

--- git_zaniatiye.py
+++ git_zaniatiye.py
@@ -4,5 +4,5 @@ def count_letters(word):
         count[letter.lower()] = count.get(letter.lower(), 0) + 1
     for key, value in count.items():
         print(key, value)
-word = 'Я не чурка'
+word = 'Абу'
 count_letters(word)
